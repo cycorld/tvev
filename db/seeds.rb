b5ece1e73a8f7bafd926734da075fc16b5fe53a5
cycorld <charleschoi87@gmail.com>
2015-08-02 04:01:12 +0900

    db/seed.rb 파일에 더미데이터 넣기 + rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tv.create(user_id: 1, title: "더지니어스", img: "http://free.tvn.cjenm.skcdn.com/CLIP/EA/B120156585/B120156585_EPI0004_11_B.jpg", intro: "[게임의 법칙], [룰 브레이커], [블랙가넷]을 거치며 불꽃 튀는 두뇌 싸움과 긴장감 넘치는 반전을 선사했던 36인의 지니어스!  그중 맹활약을 펼쳤던 13명의 지니어스가 그랜드 파이널로 돌아왔다!")
+Tv.create(user_id: 1, title: "삼시세끼", img: "http://free.tvn.cjenm.skcdn.com/CLIP/EA/B120156438/B120156438_EPI0011_06_B.jpg", intro: "순봉에 봄과 함께 찾아온 새로운 시작, '농사'.  케미 좋은 세 남자, 서진 택연 광규와 폭풍 성장한 밍키, 두 딸의 엄마가 된 잭슨, 마당으로 나오고 싶은 닭그룹이 함께 밥 짓고 농사 지으며 살어리란 이야기가 시작됩니다.")
+Tv.create(user_id: 1, title: "집밥 백선생", img: "http://free.tvn.cjenm.skcdn.com/CLIP/EA/B120156114/B120156114_EPI0011_02_B.jpg", intro: "요리불능 네 남자의 끼니해결 프로젝트 [집밥 백선생] 백선생만의 만만하고도 고급진 레시피가 대한민국을 구제하리라!")
+Tv.create(user_id: 1, title: "수요미식회", img: "http://free.tvn.cjenm.skcdn.com/CLIP/EA/B120144822/B120144822_EPI0027_02_B.jpg", intro: "맛집 줄 세우기와 단순한 먹방은 이제 그만!  먹방, 쿡방의 홍수 속 화려한 입담만으로 침샘을 자극하는 맛있는 토크쇼가 시작된다.")
