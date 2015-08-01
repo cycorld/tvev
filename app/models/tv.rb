954126c7197d1aeb36317839af36b78df1467f5d
cycorld <charleschoi87@gmail.com>
2015-08-02 05:24:28 +0900

    Tv 모델에 평균 점수 계산하는 메소드 작성
     * comments의 각 score들을 모두 합산 후
     * comments의 갯수로 나누기
     * 정확성을 위해 to_f를 사용하여 소수점 계산

--- app/models/tv.rb
+++ app/models/tv.rb
@@ -1,3 +1,11 @@
 class Tv < ActiveRecord::Base
   has_many :comments
+
+  def avg
+    total = 0
+    comments.each do |c|
+      total += c.score
+    end
+    total.to_f / comments.count
+  end
 end
