b16684efd9ceda6312aaed94a7861fca63c34e2b
cycorld <charleschoi87@gmail.com>
2015-08-02 04:50:04 +0900

    migrate 파일 수정하기
     * tv_id : 평가가 속한 tv의 id
     * user_id : 평가를 한 user의 id
     * score : 평가 점수
     * comment : 댓글

--- db/migrate/20150801194806_create_comments.rb
+++ db/migrate/20150801194806_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.integer :tv_id
+      t.integer :user_id
+      t.integer :score, default: 0
+      t.text :comment
 
       t.timestamps null: false
     end
